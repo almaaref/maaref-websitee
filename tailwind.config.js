c20d37c3e7bda1928bcaa214e29b1dd1ddd2a388
Usa96 <125459065+Usa96@users.noreply.github.com>
2025-07-14 11:11:34 +0300

    add aptos narrow fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,29 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+    content: [
+      "./*.html",            // adjust as needed
+      "./src/**/*.{js,ts}",  // if using JS/TS files in /src
+    ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['"Aptos Narrow"', 'sans-serif'],
+            },
             keyframes: {
                 fadeInUp: {
-                    '0%': { opacity: 0, transform: 'translateY(20px)' },
-                    '100%': { opacity: 1, transform: 'translateY(0)' },
-                    },
-                zoomIn: {
-                    '0%': { opacity: 0, transform: 'scale(0.9)' },
-                    '100%': { opacity: 1, transform: 'scale(1)' },
-                    },
-                },
-                animation: {
-                    'fade-in-up': 'fadeInUp 0.8s ease-out forwards',
-                    'zoom-in': 'zoomIn 0.6s ease-out forwards',
-                },
+                    '0%': { opacity: '0', transform: 'translateY(20px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+            },
+            zoomIn: {
+                '0%': { opacity: '0', transform: 'scale(0.9)' },
+                '100%': { opacity: '1', transform: 'scale(1)' },
+            },
+            },
+            animation: {
+                'fade-in-up': 'fadeInUp 0.8s ease-out forwards',
+                'zoom-in': 'zoomIn 0.6s ease-out forwards',
             },
         },
-    }
+    },
+    plugins: [],
+}
