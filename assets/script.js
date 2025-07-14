14db163153f0b12a95dd41db6ab295b17f94445a
Usa96 <125459065+Usa96@users.noreply.github.com>
2025-07-14 14:34:33 +0300

    `Added Swiper carousel to Community of Excellence section and updated related HTML and JavaScript files.`

--- assets/script.js
+++ assets/script.js
@@ -179,3 +179,18 @@ const facilitiesSwiper = new Swiper('.facilities-swiper', {
   },
 });
 
+  const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
