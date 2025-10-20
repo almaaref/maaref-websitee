33ce619f607d70d08f7ad63daefded6ef00b8ec8
Al Maaref Amercian School <schoolalmaaref@gmail.com>
2025-10-20 08:52:02 +0400

    Update news.js

--- assets/news.js
+++ assets/news.js
@@ -2,6 +2,7 @@
 const newsletters = [
 
 
+  { id: 7, title: "Newsletter #07", date: "October 16, 2025", file: "/newsletters/MAS Newsletter 7.pdf"}, 
   { id: 6, title: "Newsletter #06", date: "October 9, 2025", file: "/newsletters/MAS Newsletter 6.pdf"}, 
   { id: 5, title: "Newsletter #05", date: "October 3, 2025", file: "/newsletters/MAS Newsletter 5.pdf"},
   { id: 4, title: "Newsletter #04", date: "September 25, 2025", file: "/newsletters/MAS Newsletter4.pdf"},
