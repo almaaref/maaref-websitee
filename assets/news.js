9f5f16348c38aab3cc12d85849de03b2e69cfc59
Al Maaref Amercian School <schoolalmaaref@gmail.com>
2026-06-08 10:54:23 +0400

    Add Newsletter #34 to the newsletters array

--- assets/news.js
+++ assets/news.js
@@ -1,6 +1,7 @@
  
 const newsletters = [
 
+ { id: 34, title: "Newsletter #34", date: "June 04, 2026", file: "/newsletters/MAS Newsletter 34.pdf"},
  { id: 33, title: "Newsletter #33", date: "May 21, 2026", file: "/newsletters/MAS Newsletter 33.pdf"},
  { id: 32, title: "Newsletter #32", date: "May 14, 2026", file: "/newsletters/MAS Newsletter 32.pdf"},
  { id: 31, title: "Newsletter #31", date: "May 05, 2026", file: "/newsletters/MAS Newsletter 31.pdf"}, 
